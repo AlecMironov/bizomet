a5d978905257c5fc87bb3b2e02735967c6a6ce05
Alec Mironov <mirolex@gmail.com>
2022-01-06 22:17:30 -0500

    Initial setup

---
+++ Bizomet.Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Bizomet.Web.Data.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Bizomet.Web.Data
+{
+	public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+			: base(options, operationalStoreOptions)
+		{
+		}
+	}
+}
